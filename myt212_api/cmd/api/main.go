f5bda98da97f4f4065177233002ff281521edefd
Rodrigo Duarte <rmaduarte01@gmail.com>
2026-02-08 19:31:07 +0000

    Add all T212 API endpoints + CMD interface

--- myt212_api/cmd/api/main.go
+++ myt212_api/cmd/api/main.go
@@ -17,7 +17,7 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		fmt.Print("> ")
+		fmt.Print("\n> ")
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
 
@@ -29,8 +29,73 @@ func main() {
 				continue
 			}
 			fmt.Println(res)
-		case "quit", "exit":
-			fmt.Println("Quiting")
+		case "2":
+			res, err := t212.GetExchangesMetadata()
+			if err != nil {
+				fmt.Println("Error getting Exchanges Metadata: ", err)
+				continue
+			}
+			fmt.Println(res)
+		case "3":
+			res, err := t212.GetAvailableInstruments()
+			if err != nil {
+				fmt.Println("Error getting Available Instruments: ", err)
+				continue
+			}
+			fmt.Println(res)
+		case "4":
+			fmt.Print("Enter order ID (or leave empty for all): ")
+			orderId, _ := reader.ReadString('\n')
+			orderId = strings.TrimSpace(orderId)
+			res, err := t212.GetPendingOrders(orderId)
+			if err != nil {
+				fmt.Println("Error getting Pending Order(s): ", err)
+				continue
+			}
+			fmt.Println(res)
+		case "5":
+			fmt.Print("Enter ticker (or leave empty for all): ")
+			ticker, _ := reader.ReadString('\n')
+			ticker = strings.TrimSpace(ticker)
+			res, err := t212.GetOpenPositions(ticker)
+			if err != nil {
+				fmt.Println("Error getting Open Position(s): ", err)
+				continue
+			}
+			fmt.Println(res)
+		case "6":
+			fmt.Print("Enter pie ID (or leave empty for all): ")
+			pieId, _ := reader.ReadString('\n')
+			pieId = strings.TrimSpace(pieId)
+			res, err := t212.GetPies(pieId)
+			if err != nil {
+				fmt.Println("Error getting Pie(s): ", err)
+				continue
+			}
+			fmt.Println(res)
+		case "7":
+			res, err := t212.GetPaidOutDividends()
+			if err != nil {
+				fmt.Println("Error getting Paid Out Dividends: ", err)
+				continue
+			}
+			fmt.Println(res)
+		case "8":
+			res, err := t212.GetOrdersHistory()
+			if err != nil {
+				fmt.Println("Error getting Orders History: ", err)
+				continue
+			}
+			fmt.Println(res)
+		case "9":
+			res, err := t212.GetTransactionsHistory()
+			if err != nil {
+				fmt.Println("Error getting Transactions History: ", err)
+				continue
+			}
+			fmt.Println(res)
+		case "0", "quit", "exit":
+			fmt.Println("Quiting...")
 			return
 		default:
 			fmt.Println("Unknown command:", input)
--- myt212_api/internal/client/client.go
+++ myt212_api/internal/client/client.go
@@ -25,10 +25,251 @@ func New(cfg *config.Config) *Trading212Client {
 
 func (t212 *Trading212Client) GetAccountSummary() (string, error) {
 	reqUrl := "https://live.trading212.com/api/v0/equity/account/summary"
+
 	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
 	if err != nil {
 		return "", err
 	}
+
+	log.Println(req.Method, req.URL)
+
+	req.SetBasicAuth(t212.Cfg.APIKeyUsername, t212.Cfg.APIKeyPassword)
+	res, err := t212.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println("Status: ", res.Status)
+
+	return string(body), nil
+}
+
+func (t212 *Trading212Client) GetExchangesMetadata() (string, error) {
+	reqUrl := "https://live.trading212.com/api/v0/equity/metadata/exchanges"
+
+	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println(req.Method, req.URL)
+
+	req.SetBasicAuth(t212.Cfg.APIKeyUsername, t212.Cfg.APIKeyPassword)
+	res, err := t212.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println("Status: ", res.Status)
+
+	return string(body), nil
+}
+
+func (t212 *Trading212Client) GetAvailableInstruments() (string, error) {
+	reqUrl := "https://live.trading212.com/api/v0/equity/metadata/instruments"
+
+	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println(req.Method, req.URL)
+
+	req.SetBasicAuth(t212.Cfg.APIKeyUsername, t212.Cfg.APIKeyPassword)
+	res, err := t212.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println("Status: ", res.Status)
+
+	return string(body), nil
+}
+
+func (t212 *Trading212Client) GetPendingOrders(id string) (string, error) {
+	reqUrl := "https://live.trading212.com/api/v0/equity/orders"
+	if id != "" {
+		reqUrl += "/" + id
+	}
+
+	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println(req.Method, req.URL)
+
+	req.SetBasicAuth(t212.Cfg.APIKeyUsername, t212.Cfg.APIKeyPassword)
+	res, err := t212.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println("Status: ", res.Status)
+
+	return string(body), nil
+}
+
+func (t212 *Trading212Client) GetOpenPositions(ticker string) (string, error) {
+	reqUrl := "https://live.trading212.com/api/v0/equity/positions"
+
+	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return "", err
+	}
+
+	if ticker != "" {
+		query := req.URL.Query()
+		query.Add("ticker", ticker)
+		req.URL.RawQuery = query.Encode()
+	}
+
+	log.Println(req.Method, req.URL)
+
+	req.SetBasicAuth(t212.Cfg.APIKeyUsername, t212.Cfg.APIKeyPassword)
+	res, err := t212.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println("Status: ", res.Status)
+
+	return string(body), nil
+}
+
+func (t212 *Trading212Client) GetPies(id string) (string, error) {
+	reqUrl := "https://live.trading212.com/api/v0/equity/pies"
+	if id != "" {
+		reqUrl += "/" + id
+	}
+
+	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println(req.Method, req.URL)
+
+	req.SetBasicAuth(t212.Cfg.APIKeyUsername, t212.Cfg.APIKeyPassword)
+	res, err := t212.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println("Status: ", res.Status)
+
+	return string(body), nil
+}
+
+func (t212 *Trading212Client) GetPaidOutDividends() (string, error) {
+	reqUrl := "https://live.trading212.com/api/v0/equity/history/dividends"
+
+	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return "", err
+	}
+
+	/*query := req.URL.Query()
+	query.Add("cursor", "0")
+	query.Add("ticker", "string")
+	query.Add("limit", "21")
+	req.URL.RawQuery = query.Encode()*/
+
+	log.Println(req.Method, req.URL)
+
+	req.SetBasicAuth(t212.Cfg.APIKeyUsername, t212.Cfg.APIKeyPassword)
+	res, err := t212.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println("Status: ", res.Status)
+
+	return string(body), nil
+}
+
+func (t212 *Trading212Client) GetOrdersHistory() (string, error) {
+	reqUrl := "https://live.trading212.com/api/v0/equity/history/orders"
+
+	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println(req.Method, req.URL)
+
+	req.SetBasicAuth(t212.Cfg.APIKeyUsername, t212.Cfg.APIKeyPassword)
+	res, err := t212.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println("Status: ", res.Status)
+
+	return string(body), nil
+}
+
+func (t212 *Trading212Client) GetTransactionsHistory() (string, error) {
+	reqUrl := "https://live.trading212.com/api/v0/equity/history/transactions"
+
+	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return "", err
+	}
+
+	/*query := req.URL.Query()
+	query.Add("cursor", "0")
+	query.Add("time", "2026-01-01T00:00:00Z")
+	query.Add("limit", "21")
+	req.URL.RawQuery = query.Encode()*/
 
 	log.Println(req.Method, req.URL)
 
